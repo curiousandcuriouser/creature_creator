84133729f84fd1825809928b3a6eeca302df099a
curiousandcuriouser <204502050+curiousandcuriouser@users.noreply.github.com>
2025-06-12 20:47:15 +0200

    Define option divs

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -6,7 +6,14 @@ locationButton = document.querySelector('.js-select-location');
 useButton = document.querySelector('.js-select-use');
 worldButton = document.querySelector('.js-select-world');
 
-// Add Click
+// Define option divs
+typeOptions = document.querySelector('.js-type-options');
+animalOptions = document.querySelector('.js-animal-options');
+locationOptions = document.querySelector('.js-location-options');
+useOptions = document.querySelector('.js-use-options');
+worldOptions = document.querySelector('.js-world-options');
+
+// Add click
 
 typeButton.addEventListener('click', function() {
   alert("Hello, World!");

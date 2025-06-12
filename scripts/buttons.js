2b228f8d1baed872be45a774016509b12cd9d251
curiousandcuriouser <204502050+curiousandcuriouser@users.noreply.github.com>
2025-06-12 20:43:47 +0200

    Add classes to option divs and link button script

---
+++ scripts/buttons.js
@@ -0,0 +1,14 @@
+// Define buttons
+typeButton = document.querySelector('.js-select-type');
+console.log(typeButton);
+animalButton = document.querySelector('.js-select-animal');
+locationButton = document.querySelector('.js-select-location');
+useButton = document.querySelector('.js-select-use');
+worldButton = document.querySelector('.js-select-world');
+
+// Add Click
+
+typeButton.addEventListener('click', function() {
+  alert("Hello, World!");
+  }
+);
